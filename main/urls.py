cef77a98916f51d64b8ccc7127099e728f53e2ae
cryptonoob12 <cryptonoob12@gmail.com>
2018-02-26 23:42:20 +0000

    First commit

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+from . import views
+app_name = "main"
+urlpatterns = [
+    url(r'^$', views.index,name="home"),
+    url(r'^detail/(?P<pkd>[0-9]+)/',views.ico_detail,name = "detail"),
+    url(r'^category/(?P<category>\w{0,50})/',views.show_by_category,name="category"),
+    url(r'^search/',views.search_ico,name="search"),
+    url(r'^about/',views.about,name="about"),
+    url(r'^adver/',views.adver,name="adver"),
+]
